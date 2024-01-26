425e8eb745840f57ca0b14789e22433c907d8bd6
nowi1GG <alejandrorafaelfr@gmail.com>
2024-01-26 16:54:03 -0400

    ultima-version 26/01/24

---
+++ Controllers/Compartidos.php
@@ -0,0 +1,27 @@
+<?php
+class Compartidos extends Controller
+{
+    private $id_usuario;
+    public function __construct()
+    {
+        parent::__construct();
+        session_start();
+        $this->id_usuario = $_SESSION['id'];
+    }
+    public function index()
+    {
+        $data['title'] = 'Archivos compartidos';
+        $data['script'] = 'compartidos.js';
+        $correo = $_SESSION['correo'];
+        $data['archivos'] = $this->model->getArchivosCompartidos($correo);
+        $this->views->getView('admin', 'compartidos', $data);
+    }
+
+    public function verDetalle($id_detalle)
+    {
+        $data = $this->model->getDetalle($id_detalle);
+        $data['fecha'] = time_ago(strtotime($data['fecha_add']));
+        echo json_encode($data);
+        die();
+    }
+}
