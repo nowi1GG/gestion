806e994c44506828587cb17693775eefd4b47437
nowi1GG <alejandrorafaelfr@gmail.com>
2024-02-12 18:36:43 -0400

    v2.1 INICIO CUARTA PARTE

--- Views/admin/archivos.php
+++ Views/admin/archivos.php
@@ -29,8 +29,8 @@
                                     <a href="#" class="dropdown-toggle file-manager-recent-file-actions" id="file-manager-recent-1" data-bs-toggle="dropdown" aria-expanded="false"><i class="material-icons">more_vert</i></a>
                                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="file-manager-recent-1">
                                         <li><a class="dropdown-item" href="#">Compartir</a></li>
-                                        <li><a class="dropdown-item" href="#">Download</a></li>
-                                        <li><a class="dropdown-item" href="#">Move to folder</a></li>
+                                        <li><a class="dropdown-item" href="#">Descargar</a></li>
+                                        <li><a class="dropdown-item" href="#">Eliminar</a></li>
                                     </ul>
                                 </div>
                             </div>
